8f80d7f4418cc52f68563e1aa83d60b7a614a522
Mehdi Taibi <mr.mehdi.taibi@gmail.com>
2018-11-17 16:59:34 -0800

    add forward(), right(), left() and allpoints()

---
+++ box_it_script/turtle_graphics/turtle.js
@@ -0,0 +1,75 @@
+
+class Turtle {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.coordinates = [];
+        this.orientation = 'East';
+    };
+
+    forward(num){
+        (this.orientation === 'East') ? this.x += num:
+        (this.orientation === 'South') ? this.y += num:
+        (this.orientation === 'West') ? this.x -= num:
+        (this.orientation === 'North') ? this.y -= num:
+        'This frog doesn\'t know where to go';
+
+        this.coordinates.push([this.x, this.y]);
+    };
+
+    right(){
+        switch(this.orientation){
+            case 'North':
+            this.orientation = 'East';
+            break;
+            case 'East':
+            this.orientation = 'South';
+            break;
+            case 'South':
+            this.orientation = 'West';
+            break;
+            case 'West':
+            this.orientation = 'North';
+            default:
+            this.orientation = 'East'
+        }
+    };
+
+    left(){
+        switch(this.orientation){
+            case 'North':
+            this.orientation = 'West';
+            break;
+            case 'West':
+            this.orientation = 'South';
+            break;
+            case 'South':
+            this.orientation = 'East';
+            break;
+            case 'East':
+            this.orientation = 'North';
+            default:
+            this.orientation = 'East'
+        }
+    };
+
+    allPoints(){ 
+        return this.coordinates;
+    };
+};
+
+
+let Sonic = new Turtle(0, 0)
+  Sonic.forward(5)
+  Sonic.right()
+  Sonic.forward(5)
+  Sonic.right()
+  Sonic.forward(5)
+  Sonic.right()
+  Sonic.forward(5)
+  Sonic.allPoints()
+
+
+
+
+
