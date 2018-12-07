751c96ba2213c5c28dc8c3e6914d948c579a034b
Mehdi Taibi <mr.mehdi.taibi@gmail.com>
2018-12-06 18:14:24 -0800

    remove super_team_picker

--- super_team_picker/db/client.js
+++
@@ -1,8 +0,0 @@
-
-const knexfile = require('../knexfile');
-
-const knexConnector = require('knex');
-
-const knex = knexConnector(knexfile.development);
-
-module.exports = knex;
--- super_team_picker/routes/base.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
- router.get('/', (request, response) => {
-  response.render('home');
-});
-
- module.exports = router;
